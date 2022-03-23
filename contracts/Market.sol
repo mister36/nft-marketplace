4d99ca319a68fd1f8cce49d989027e3d93b20502
Adam Achebe <mister363636@gmail.com>
2022-03-23 16:44:38 -0500

    Added sale functionality

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -7,7 +7,7 @@ contract Market {
   struct Listing {
     address token;
     address seller;
-    uint256 id;
+    uint256 tokenId;
     uint256 price;
     ListingStatus status;
   }
@@ -17,6 +17,25 @@ contract Market {
     Sold,
     Cancelled
   }
+
+  event Listed(
+    address seller,
+    address token,
+    uint256 tokenId,
+    uint256 price,
+    uint256 listingId
+  );
+
+  event Sale(
+    address seller,
+    address buyer,
+    address token,
+    uint256 tokenId,
+    uint256 price,
+    uint256 listingId
+  );
+
+  event Cancel(address seller, address token, uint256 tokenId);
 
   uint256 private _listingId = 0;
   mapping(uint256 => Listing) public _listings;
@@ -28,6 +47,8 @@ contract Market {
     uint256 _id,
     uint256 _price
   ) external {
+    IERC721(_token).transferFrom(msg.sender, address(this), _id);
+
     Listing memory listing = Listing(
       _token,
       msg.sender,
@@ -37,6 +58,8 @@ contract Market {
     );
 
     _listings[_listingId++] = listing;
+
+    emit Listed(msg.sender, _token, _id, _price, _listingId);
   }
 
   function buyToken(uint256 _id) external payable {
@@ -44,9 +67,41 @@ contract Market {
 
     require(listing.status == ListingStatus.Active, "Listing must be active");
     require(msg.sender != listing.seller, "Cannot buy your own listing");
-    require(
-      msg.value == listing.price,
-      "Ether sent must be equal the price of the listing"
+    require(msg.value >= listing.price, "Insufficient funds");
+
+    IERC721(listing.token).transferFrom(
+      address(this),
+      msg.sender,
+      listing.tokenId
     );
+    payable(listing.seller).transfer(listing.price);
+
+    emit Sale(
+      listing.seller,
+      msg.sender,
+      listing.token,
+      listing.tokenId,
+      listing.price,
+      _id
+    );
+  }
+
+  function cancel(uint256 _id) public {
+    Listing storage listing = _listings[_id];
+
+    require(listing.status == ListingStatus.Active, "Listing is not active");
+    require(
+      msg.sender == listing.seller,
+      "Must be the owner to cancel listing"
+    );
+
+    listing.status = ListingStatus.Cancelled;
+    IERC721(listing.token).transferFrom(
+      address(this),
+      msg.sender,
+      listing.tokenId
+    );
+
+    emit Cancel(msg.sender, listing.token, listing.tokenId);
   }
 }
